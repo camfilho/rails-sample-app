4684f728e53e61e497afbc3df1360e9b9ac2921f
camfilho <camfilho@ufrn.edu.br>
2020-01-06 18:13:14 -0300

    create signup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/help', to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users
 end
